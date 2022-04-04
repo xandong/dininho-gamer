81b0c4760e41eb8047f86d968b30f43431e91077
Luan <luan@plannerti.com.br>
2022-04-04 11:56:50 -0300

    upload backend made in .NET

---
+++ back/GameAPI/Controllers/GameController.cs
@@ -0,0 +1,36 @@
+﻿using GameAPI.Models;
+using GameAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly IGameRepository _gameRepository;
+
+        public GameController(IGameRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ResultGame>>> GetResultGames()
+        {
+            var resultGames = await _gameRepository.GetResultgames();
+            return Ok(resultGames);
+        }
+        [HttpPost]
+        public async Task<ActionResult<ResultGame>> InsertResultGame(ResultGame resultGame)
+        {
+            var lResultGame = await _gameRepository.Create(resultGame);
+            return Ok(lResultGame);
+
+        }
+
+    }
+}
---
+++ back/GameAPI/Models/ResultGame.cs
@@ -0,0 +1,9 @@
+﻿namespace GameAPI.Models
+{
+    public class ResultGame
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public string DateOfGame { get; set; }
+    }
+}
