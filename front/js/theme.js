7146438de81090a2cd6994a5585cd4820b672a0a
ventusdigital <94716639+xandong@users.noreply.github.com>
2022-04-09 04:21:44 -0300

    chore(theme): alter mode for theme.

--- front/js/theme.js
+++ front/js/theme.js
@@ -1,27 +1,26 @@
-const modeMain = document.querySelector("#mode");
-const stateMode = document.querySelector("#state-mode");
+const theme = document.querySelector("#theme");
+const stateMode = document.querySelector("#state-theme");
 
 let mode = false;
 
 function click() {
-  console.log("CLICOU");
   if (!mode) {
     mode = true;
     document.body.style.color = "whitesmoke";
     document.body.style.background = "black";
 
-    modeMain.style.justifyContent = "end";
-    modeMain.style.background = "whitesmoke";
+    theme.style.justifyContent = "end";
+    theme.style.background = "whitesmoke";
     stateMode.style.background = "rebeccapurple";
   } else {
     mode = false;
     document.body.style.color = "black";
     document.body.style.background = "whitesmoke";
 
-    modeMain.style.justifyContent = "start";
-    modeMain.style.background = "#3A026A";
+    theme.style.justifyContent = "start";
+    theme.style.background = "#3A026A";
     stateMode.style.background = "whitesmoke";
   }
 }
 
-modeMain.addEventListener("click", click, false);
+theme.addEventListener("click", click, false);
